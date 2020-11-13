8eef37f241ec9f80ff33a56ffbb43f06abed6f76
apinkev <apinkevich@yahoo.com>
2020-11-13 14:31:11 +0100

    adding env support

--- src/sagas/fetchApi.js
+++ src/sagas/fetchApi.js
@@ -1,13 +1,14 @@
 import md5 from "md5";
 
-const PRIVATE_API_KEY = "ffcd2b1d9ac49164c339dfdc36c1182409870d62";
-const PUBLIC_API_KEY = "9d9c21438275446a33bbafced69fbcd9";
+const { REACT_APP_PRIVATE_API_KEY, REACT_APP_PUBLIC_API_KEY } = process.env;
 
 async function fetchApi(query) {
     const baseUrl = "https://gateway.marvel.com/v1/public/series";
     const timestamp = new Date().getTime();
-    const hash = md5(timestamp + PRIVATE_API_KEY + PUBLIC_API_KEY);
-    const auth = `?ts=${timestamp}&apikey=${PUBLIC_API_KEY}&hash=${hash}`;
+    const hash = md5(
+        timestamp + REACT_APP_PRIVATE_API_KEY + REACT_APP_PUBLIC_API_KEY
+    );
+    const auth = `?ts=${timestamp}&apikey=${REACT_APP_PUBLIC_API_KEY}&hash=${hash}`;
     const url = query ? `${baseUrl}?${query}${auth}` : `${baseUrl}${auth}`;
 
     try {
