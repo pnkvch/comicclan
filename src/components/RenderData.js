61988d6a1953de2b81548761e53edc45ba8391aa
apinkev <apinkevich@yahoo.com>
2021-03-03 23:06:56 +0200

    alfabetical sorting

--- src/components/RenderData.js
+++ src/components/RenderData.js
@@ -3,6 +3,22 @@ import BooksSlider from "./BooksSlider";
 import { PendingWrapper } from "../style/styles";
 import { filterPlainArray, getUniqueValues, sortComics } from "./utils";
 
+const sortByFirstLetter = (arr, letter) => {
+  let firstCharacter, itemIdx;
+  const newArr = arr.filter((item, index) => {
+    if (item.startsWith(letter)) {
+      itemIdx = index;
+    }
+    return item.startsWith(letter);
+  });
+  if (arr[itemIdx + 1]) {
+    firstCharacter = arr[itemIdx + 1].charAt(0);
+  }
+  console.log(newArr, itemIdx, firstCharacter);
+
+  return [newArr, firstCharacter];
+};
+
 const RenderData = ({ comics, criteria }) => {
   if (!comics?.length) {
     return <PendingWrapper>There is no match</PendingWrapper>;
@@ -10,7 +26,11 @@ const RenderData = ({ comics, criteria }) => {
 
   const sortedComics = sortComics(comics, criteria);
   const unique = getUniqueValues(sortedComics, criteria);
-  let heading, filters, filteredArray;
+  let heading,
+    filters,
+    filteredArray,
+    letter = unique[0].charAt(0),
+    newChar;
 
   if (criteria === "random") {
     return (
@@ -26,13 +46,31 @@ const RenderData = ({ comics, criteria }) => {
       [criteria]: [item]
     };
 
-    filteredArray = filterPlainArray(sortedComics, filters);
-    heading = filteredArray[0][criteria];
+    console.log(letter, unique);
+
+    if (criteria === "title") {
+      [filteredArray, newChar] = sortByFirstLetter(unique, letter);
+      console.log(filteredArray);
+      letter = newChar;
+    } else {
+      filteredArray = filterPlainArray(sortedComics, filters);
+      heading = filteredArray[0][criteria];
+    }
+    let idx = 0,
+      i;
+
+    const asa = sortedComics.filter((item, index) => {
+      if (item.title === filteredArray[idx]) {
+        i = idx;
+        idx++;
+      }
+      return item.title === filteredArray[i];
+    });
 
     return (
       <div key={index}>
-        <h2>{heading}</h2>
-        <BooksSlider comics={filteredArray} />
+        <h2>{letter}</h2>
+        <BooksSlider comics={asa} />
       </div>
     );
   });
