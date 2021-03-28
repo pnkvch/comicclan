2f449625fc04fcd13e439a05166b8cfbcf6b0cb1
apinkev <apinkevich@yahoo.com>
2021-03-29 00:01:46 +0200

    added axios

--- src/sagas/fetchApi.js
+++ src/sagas/fetchApi.js
@@ -1,11 +1,13 @@
+import axios from "axios";
+
 export async function fetchData(query) {
   const url = query
     ? `https://api.itbook.store/1.0/search/${query}`
     : "https://api.itbook.store/1.0/new";
 
   try {
-    const response = await fetch(url).then(x => x.json());
-    const results = response.books;
+    const response = await axios.get(url);
+    const results = response.data.books;
 
     return results;
   } catch (e) {
@@ -17,8 +19,8 @@ export async function fetchBookData(query) {
   const url = `https://api.itbook.store/1.0/books/${query}`;
 
   try {
-    const response = await fetch(url).then(x => x.json());
-    const results = response;
+    const response = await axios.get(url);
+    const results = response.data;
 
     return results;
   } catch (e) {
